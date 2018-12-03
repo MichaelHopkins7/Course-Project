c28e75292139d7a84ff6c14016d929c88d28db65
Michael Hopkins <LuckyMagnet77@gmail.com>
2018-12-03 10:03:04 -0500

    Moved ERD/SRD to correct location Added Entities for class and character

---
+++ code/CharacterMaker.Domain/Abstract/ClassRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CharacterMaker.Domain.Abstract
+{
+    interface CharClassRepository
+    {
+        IEnumerable<CharClass> CharClasses { get; }
+    }
+}
---
+++ code/CharacterMaker.Domain/Entities/CharClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CharacterMaker.Domain.Entities
+{
+    class CharClass
+    {
+        public int HDSize {get; set; }
+        public int BABGrowth { get; set; }
+        public int FortGrowth { get; set; }
+        public int ReflexGrowth { get; set; }
+        public int WillGrowth { get; set; }
+        public int WeaponProf { get; set; }
+        public int ArmorProf { get; set; }
+        public int ShieldProf { get; set; }
+        public string SpecialProf { get; set; }
+
+    }
+}
