489b1fffe3a032b0eb32892b01c7d8ba6e396c47
Michael Hopkins <LuckyMagnet77@gmail.com>
2018-12-06 11:19:19 -0500

    Added code for ability mod calculations

--- code/CharacterMaker.Domain/Entities/Character.cs
+++ code/CharacterMaker.Domain/Entities/Character.cs
@@ -8,13 +8,19 @@ namespace CharacterMaker.Domain.Entities
     {
 
         public string CharName { get; set; }
-        public string Race { get; set; } 
+        public string Race { get; set; }
         public int Strength { get; set; }
+        public int StrMod{ get; set; }
         public int Dexterity { get; set; }
+        public int DexMod { get; set; }
         public int Constitution { get; set; }
+        public int ConMod { get; set; }
         public int Inteligence { get; set; }
+        public int IntMod { get; set; }
         public int Wisdom { get; set; }
+        public int WisMod { get; set; }
         public int Charisma { get; set; }
+        public int ChaMod { get; set; }
         public int CharLvl { get; set; }
         public int HP { get; set; }
         public int BAB { get; set; }
@@ -22,5 +28,21 @@ namespace CharacterMaker.Domain.Entities
         public int Reflex { get; set; }
         public int Will { get; set; }
 
+        public int GetAbilityMod(int abilityScore)
+        {
+            return (abilityScore / 2) - 5;
+        }
+
+        public int GetPointCost(int abilityScore)
+        {
+            int pointcost = -6;
+            int accumulator;
+            for (int i = 7; i <= abilityScore; i++)
+{
+                accumulator = Math.Abs((i % 2) - 5);
+                pointcost += accumulator;
+            }
+            return pointcost;
+        }
     }
 }
